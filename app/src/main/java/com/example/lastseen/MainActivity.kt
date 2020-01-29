eb6333b01277de029da64f779ffa9eca5ecbcca6
unknown <cynoxsis@gmail.com>
2020-01-29 15:50:05 -0700

    Update Information page layout

--- app/src/main/java/com/example/lastseen/MainActivity.kt
+++ app/src/main/java/com/example/lastseen/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.example.lastseen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == R.id.update_information) {
+            val intent = Intent(this, UpdateInformation::class.java)
+
+            startActivity(intent)
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/example/lastseen/UpdateInformation.kt
@@ -0,0 +1,12 @@
+package com.example.lastseen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UpdateInformation : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_information)
+    }
+}
