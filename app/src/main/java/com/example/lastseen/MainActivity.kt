265f2aaac98703966620d2f53e12d3af069ad6f9
Luis <lruizarc@msudenver.edu>
2020-03-01 19:41:07 -0700

    Added check for user method

--- app/src/main/java/com/example/lastseen/MainActivity.kt
+++ app/src/main/java/com/example/lastseen/MainActivity.kt
@@ -3,30 +3,24 @@ package com.example.lastseen
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.text.TextUtils
-import android.util.Log
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
-import com.firebase.ui.auth.viewmodel.RequestCodes
-import com.google.android.gms.auth.api.Auth
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.SignInButton
-import com.google.android.gms.common.api.ApiException
-
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.itinerary.*
 import java.util.*
+
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
-    private lateinit var mGoogleSignInClient: GoogleSignInClient
     private lateinit var providers : List<AuthUI.IdpConfig>
 
 
@@ -41,8 +35,26 @@ class MainActivity : AppCompatActivity() {
         )
         showSignInOptions()
 
+    }
 
+    private fun checkIfUserExists(user : FirebaseUser?){
+        val queue = Volley.newRequestQueue(this)
+        val urlString = getString(R.string.server_url) + getString(R.string.server_profile) + user?.uid
 
+        val getProfileRequest = JsonObjectRequest(
+            Request.Method.GET, urlString, null,
+            Response.Listener {
+                val intent = Intent(this, Itinerary::class.java)
+                intent.putExtra("userIdentifier", user?.uid)
+
+                startActivity(intent)
+                finish()
+            },
+            Response.ErrorListener {
+                Toast.makeText(applicationContext, "NO TREK POINT ACCOUNT FOUND FOR GOOGLE ACCOUNT", Toast.LENGTH_SHORT).show()
+            })
+
+        queue.add(getProfileRequest)
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
         super.onActivityResult(requestCode, resultCode, data)
@@ -51,6 +63,7 @@ class MainActivity : AppCompatActivity() {
             if (resultCode == Activity.RESULT_OK){
                 val user = FirebaseAuth.getInstance().currentUser
                 Toast.makeText(this, ""+user!!.email,Toast.LENGTH_LONG).show()
+
             }
             else{
                 Toast.makeText(this, ""+response!!.error!!.message,Toast.LENGTH_LONG).show()
@@ -65,7 +78,9 @@ class MainActivity : AppCompatActivity() {
             .build(), RC_SIGN_IN)
     }
 
+
     companion object{
         const val RC_SIGN_IN = 1234
     }
+
     }
