134b5b6a029573a92911104554f37205cb6b9b5f
Luis <lruizarc@msudenver.edu>
2020-03-01 19:43:44 -0700

    fixed Intent issue

--- app/src/main/java/com/example/lastseen/MainActivity.kt
+++ app/src/main/java/com/example/lastseen/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.itinerary)
 
 
         providers = Arrays.asList<AuthUI.IdpConfig>(
